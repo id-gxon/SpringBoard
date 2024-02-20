df20df114317c17f3e94a0a4bf53a59a23daa917
id-gxon <wskgw123@gmail.com>
2024-02-20 17:50:18 +0900

    도메인 객체 생성 (BoardVO)

---
+++ SpringBoard/src/main/java/com/itwillbs/domain/BoardVO.java
@@ -0,0 +1,15 @@
+package com.itwillbs.domain;
+
+import java.sql.Timestamp;
+
+import lombok.Data;
+
+@Data
+public class BoardVO {
+	private int bno;
+	private String title;
+	private String context;
+	private String writer;
+	private int viewcnt;
+	private Timestamp regdate;
+}
